41ed79f892d93ae35b6750e3a67ab91c99709957
FCBleomessi10 <710823459@qq.com>
2023-04-29 12:07:49 +0800

    consumer80消费者订单模块

---
+++ cloud-consumer-order80/src/main/java/com/shu/springcloud/entities/CommonResult.java
@@ -0,0 +1,14 @@
+package com.shu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResult<T> {
+    private Integer code;
+    private String message;
+    private T data;
+}
