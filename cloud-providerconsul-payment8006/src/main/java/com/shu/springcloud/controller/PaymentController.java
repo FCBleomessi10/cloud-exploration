977bbc8cb62015d6bc7b355b6cadf352ab5eabd8
FCBleomessi10 <710823459@qq.com>
2023-05-03 17:11:34 +0800

    使用Consul作为支付微服务的注册中心

---
+++ cloud-providerconsul-payment8006/src/main/java/com/shu/springcloud/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.shu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/consul")
+    public String paymentZk() {
+        return "springcloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID();
+    }
+}
