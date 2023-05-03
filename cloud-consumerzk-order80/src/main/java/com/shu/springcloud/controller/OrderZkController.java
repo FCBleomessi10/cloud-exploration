c49b595c3527c753cdd5689d722f0e7c0d2cac18
FCBleomessi10 <710823459@qq.com>
2023-05-03 16:40:12 +0800

    使用Zookeeper作为支付微服务的注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/shu/springcloud/controller/OrderZkController.java
@@ -0,0 +1,21 @@
+package com.shu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderZkController {
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zk")
+    public String paymentInfo() {
+        return restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+    }
+}
