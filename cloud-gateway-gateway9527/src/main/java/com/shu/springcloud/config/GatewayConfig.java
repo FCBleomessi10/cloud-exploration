ebb97f018d2935d0e4c7b8486010662b6188b45f
FCBleomessi10 <710823459@qq.com>
2023-05-08 10:54:10 +0800

    使用Gateway配置路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/shu/springcloud/config/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.shu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
